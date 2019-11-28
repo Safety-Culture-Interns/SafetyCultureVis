2b5353aed5c7af47da1209ed75e0267598348159
JoshuaGale <joshua.gale@my.jcu.edu.au>
2019-11-28 16:28:26 +1000

    add generated dates to the csv writer

--- WriteCsv.py
+++ WriteCsv.py
@@ -1,5 +1,9 @@
 import csv
+import random
+import datetime
 from audits.Audits import Audit
+
+LONG_MONTHS = [1, 3, 5, 7, 8, 10, 12]
 
 
 def write_to_csv(audits):
@@ -12,9 +16,10 @@ def write_to_csv(audits):
     # first line of the csv is titles
 
     for i, audit in enumerate(audits):
-        audit_created_at = str(audits[i].get_audit_created_at())
-        audit_modified_at = str(audits[i].get_audit_modified_at())
-        date_completed = str(audits[i].get_date_completed())
+
+        audit_created_at = generate_date("2000-01-01T00:00:00.000Z")
+        audit_modified_at = generate_date(audit_created_at)
+        date_completed = generate_completed_datetime(audit_modified_at, audits[i].get_duration())
 
         csv_data.append([audits[i].get_audit_id(), audits[i].get_template_id(), audits[i].get_archived(),
                          audit_created_at[:10], audit_created_at[11:-5], audit_modified_at[:10], audit_modified_at[11:-5],
@@ -30,3 +35,44 @@ def write_to_csv(audits):
         writer = csv.writer(csvFile)
         writer.writerows(csv_data)
     csvFile.close()
+
+
+def generate_date(earlier_string):
+    correct_datetime = False
+    early_values = [int(earlier_string[:4]), int(earlier_string[5:7]), int(earlier_string[8:10]),
+                    int(earlier_string[11:13]), int(earlier_string[14:16]), int(earlier_string[17:19])]
+    early_datetime = datetime.datetime(early_values[0], early_values[1], early_values[2], early_values[3],
+                                       early_values[4], early_values[5])
+
+    while not correct_datetime:
+        seconds = random.randrange(0, 59)
+        minuets = random.randrange(0, 59)
+        hour = random.randrange(0, 24)
+        year = random.randrange(2010, 2019)
+        month = random.randrange(1, 12)
+        if month in LONG_MONTHS:
+            max_days = 31
+        elif month == 2:
+            max_days = 28
+        else:
+            max_days = 30
+        day = random.randrange(1, max_days)
+        current_datetime = datetime.datetime(year, month, day, hour, minuets, seconds)
+        if current_datetime > early_datetime:
+            correct_datetime = True
+            date_time = "{}-{:02d}-{:02d}T{:02d}:{:02d}:{:02d}.000Z".format(year, month, day, hour, minuets, seconds)
+    return date_time
+
+
+def generate_completed_datetime(date_string, duration):
+    early_values = [int(date_string[:4]), int(date_string[5:7]), int(date_string[8:10]),
+                    int(date_string[11:13]), int(date_string[14:16]), int(date_string[17:19])]
+    early_datetime = datetime.datetime(early_values[0], early_values[1], early_values[2], early_values[3],
+                                       early_values[4], early_values[5])
+    completed_datetime = early_datetime + datetime.timedelta(seconds=duration)
+    date_time = "{}-{:02d}-{:02d}T{:02d}:{:02d}:{:02d}.000Z".format(completed_datetime.year, completed_datetime.month,
+                                                                    completed_datetime.day, completed_datetime.hour,
+                                                                    completed_datetime.minute,
+                                                                    completed_datetime.second)
+
+    return date_time
