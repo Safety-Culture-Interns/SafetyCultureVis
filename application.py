ba5fd234a1c1cb407575dafdbd08d11c37f3c09e
Nathan Marson <nathan.marson@my.jcu.edu.au>
2019-12-12 11:47:35 +1000

    update server variable name to application

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 # import flask server and create it
 from flaskr import create_app
 
-server = create_app()
+application = create_app()
 
 if __name__ == "__main__":
-    server.run(debug=True)
+    application.run(debug=True)
