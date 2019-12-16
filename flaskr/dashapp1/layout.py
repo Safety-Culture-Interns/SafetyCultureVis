0ea26e9b0e3873a90f373d54877b6e1760e73cf1
Matthew <matt.lewandowski@my.jcu.edu.au>
2019-12-16 11:12:35 +1000

    style changes to main dashboard

--- flaskr/dashapp1/layout.py
+++ flaskr/dashapp1/layout.py
@@ -12,32 +12,39 @@ current_date = datetime.datetime.now()
 previous_date = current_date + datetime.timedelta(-29)
 
 location_map = html.Div([
-    html.Div(children=[
-        dcc.DatePickerRange(
-            id='date-picker-range',
-            start_date=previous_date.date(),
-            end_date=current_date.date()
-        )], style={'width': '35%', 'display': 'inline-block', 'text-align': 'center', 'margin': '0 auto'}),
-    html.Div(children=[
-        dcc.Dropdown(options=[
-            {'label': 'Audit Created', 'value': 'Created_at'},
-            {'label': 'Audit Modified', 'value': 'Modified_at'},
-            {'label': 'Date Completed', 'value': 'completed_at'}
-        ], value='Created_at',
-            id='date-sort')],
-        style={'width': '100%', 'display': 'inline-block', 'text-align': 'center', 'margin': '0 auto'}),
-    dcc.Graph(
-        id='map',
-        style={'width': '100%'},
-        config={
-            'displayModeBar': False,
-            'autosizable': True,
-            'fillframe': True,
-            'colorBarPosition': True,
-            'titleText': False
-        }
-    ),
-], style={'width': '100%', 'display': 'flex', 'flex-wrap': 'wrap', 'margin-top': '10px'})
+    html.Div([
+        html.Div(children=[
+            dcc.DatePickerRange(
+                id='date-picker-range',
+                start_date=previous_date.date(),
+                end_date=current_date.date(),
+            )], className='pretty_container',
+            style={'width': '50%', 'text-align': 'center'}),
+        html.Div(children=[
+            dcc.Dropdown(options=[
+                {'label': 'Audit Created', 'value': 'Created_at'},
+                {'label': 'Audit Modified', 'value': 'Modified_at'},
+                {'label': 'Date Completed', 'value': 'completed_at'}
+            ], value='Created_at',
+                id='date-sort')],
+            className='pretty_container',
+            style={'width': '50%'}),
+    ], style={'display': 'flex'}),
+    html.Div([
+        dcc.Graph(
+            id='map',
+            style={'width': '100%'},
+            config={
+                'displayModeBar': False,
+                'autosizable': True,
+                'fillframe': True,
+                'colorBarPosition': True,
+                'titleText': False
+            }
+        ),
+    ], className='pretty_container'),
+
+], id='right-column', className='eight columns')
 score_gauge = html.Div([
     dcc.Input(
         id='fake-input',
@@ -54,24 +61,31 @@ score_gauge = html.Div([
         max=100,
         min=0,
     ),
-], style={'border-width': '5px', 'border-style': 'solid', 'width': '85%', 'position': 'relative'})
-
+], id='score_guage', className='pretty_container four columns')
 
 # title of the sidebar
 sidebar_header = html.Div([
     dbc.Row(html.Div('Dashboard', className="active", style={'padding': '12%'}), className="active")
 ])
+# map and health bar
+map_health_bar = html.Div([
+    score_gauge, location_map
+], className='flex-display')
 
-# sidebar of the homepage
-sidebar = html.Div([
-    sidebar_header,
-    dbc.Row(html.A('haha1', style={'padding': '15%'})),
-    dbc.Row(html.A('haha2', style={'padding': '15%'})),
-    dbc.Row(html.A('haha3', style={'padding': '15%'}))
-], className="sidebar", style={'position': 'relative', 'height': 'auto', 'display': 'block'})
+# header bar
+header = html.Div([
+    html.Div([
+        html.Img(src='iauditor-badge.png')
+    ], className='one-third column'),
+    html.Div([
+        html.H1('Safety Culture Dashboard')
+    ], className='one-half column', id='title'),
+    html.Div([
+    ], className='one-third column'),
+], className='row flex-display', id='header', style={'margin-bottom': '25px'})
 
 # main layout
 layout = html.Div([
-    sidebar,
-    score_gauge, location_map
-], style={'display': 'flex', 'flex-wrap': 'wrap', 'width': '100%', 'height': '100%'})
+    header,
+    map_health_bar
+], id='mainContainer')
